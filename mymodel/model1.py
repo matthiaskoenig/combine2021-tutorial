c594be243c51648c831448d9ddba3bf4c7700c6f
Matthias Koenig <konigmatt@googlemail.com>
2021-10-13 14:41:16 +0200

    annotations added

--- mymodel/model1.py
+++ mymodel/model1.py
@@ -15,14 +15,30 @@ _m = Model(
     This model was built within the **tutorial session**.
     """,
     compartments=[
-        Compartment(sid="c", value=1.0, sboTerm=SBO.PHYSICAL_COMPARTMENT,
-                    name="cytosol"),
+        Compartment(
+            sid="c", value=1.0, sboTerm=SBO.PHYSICAL_COMPARTMENT, name="cytosol",
+            annotations=[
+                (BQB.IS, "ncit/C61554"),
+                (BQB.IS, "GO:0005829"),
+                (BQB.IS, "FMA:66836"),
+            ]
+        ),
     ],
     species=[
-        Species(sid="glc", compartment="c", initialConcentration=10.0, hasOnlySubstanceUnits=False,
-                sboTerm=SBO.SIMPLE_CHEMICAL,
-                name="glucose")
-    ]
+        Species(
+            sid="glc",
+            compartment="c",
+            initialConcentration=10.0,
+            hasOnlySubstanceUnits=False,
+            sboTerm=SBO.SIMPLE_CHEMICAL,
+            name="glucose",
+            annotations=[
+                  (BQB.IS, "ncit/C2831"),
+                (BQB.IS, "CHEBI:17234"),
+
+            ]
+        )
+    ],
 )
 
 
@@ -32,4 +48,7 @@ if __name__ == "__main__":
         output_dir=Path(__file__).parent / "results",
         units_consistency=False,
     )
-    print("Report: ", "https://sbml4humans.de/model_url?url=http://be86-77-13-36-21.ngrok.io/model1.xml")
+    print(
+        "Report: ",
+        "https://sbml4humans.de/model_url?url=http://be86-77-13-36-21.ngrok.io/model1.xml",
+    )
